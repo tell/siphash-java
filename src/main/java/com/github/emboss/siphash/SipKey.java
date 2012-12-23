c08edc0a76c4debb210d79a411b9644458966a4e
Tadanori TERUYA <tadanori.teruya@gmail.com>
2012-12-23 22:52:28 +0900

    implement Key interface

--- src/main/java/com/github/emboss/siphash/SipKey.java
+++ src/main/java/com/github/emboss/siphash/SipKey.java
@@ -1,23 +1,39 @@
 package com.github.emboss.siphash;
 
+import java.security.Key;
+
 /**
- * 
  * @author <a href="mailto:Martin.Bosslet@googlemail.com">Martin Bosslet</a>
  */
-public class SipKey {
+public class SipKey implements Key {
     private final byte[] key;
-    
+
     public SipKey(byte[] key) {
         if (key == null || key.length != 16)
             throw new RuntimeException("SipHash key must be 16 bytes");
         this.key = key;
     }
-    
+
     long getLeftHalf() {
-       return UnsignedInt64.binToIntOffset(key, 0); 
+        return UnsignedInt64.binToIntOffset(key, 0);
     }
-    
+
     long getRightHalf() {
-        return UnsignedInt64.binToIntOffset(key, 8); 
+        return UnsignedInt64.binToIntOffset(key, 8);
+    }
+
+    @Override
+    public String getAlgorithm() {
+        return "SipHash-2-4";
+    }
+
+    @Override
+    public String getFormat() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public byte[] getEncoded() {
+        return key;
     }
 }
